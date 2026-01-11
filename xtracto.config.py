9fbcbfec81ad7e4771497c84a3a5269d767049c0
shashstormer <shashanka5398@gmail.com>
2026-01-11 17:26:57 +0530

    prod = True for git

--- xtracto.config.py
+++ xtracto.config.py
@@ -2,5 +2,5 @@ modules_dir = "components" # Directory for reusable components
 pages_dir = "pages"       # Directory for your pages
 build_dir = "build"              # Directory for built HTML files
 log_level = "critical"               # Logging level
-production = False         # Production mode flag
+production = True         # Production mode flag
 reparse_tailwind = False         # Whether to regenerate Tailwind CSS on render
